ee354b74edc4845720c8c96e31562b8efcb040f0
Sara-Alcaras <saraalcaras401@gmail.com>
2020-05-30 00:16:25 -0300

    api
    "consumindo a api"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+
+class App extends Component {
+    state = {
+            posts: [],
+            assetsDatas: [],
+
+        }
+    
+    
+     async componentDidMount(){
+        const response = await axios.get('https://tractian-data.s3.us-east-2.amazonaws.com/api.json')
+       
+        this.setState({posts: response.data.units})
+        this.setState({assetsDatas: response.data.units})
+//        console.log(this.assetsDatas)
+    
+
+    
+       
+       //this.setState({posts: response.data.units})
+       //this.setState({assetsDatas: response.data.units.data.assetsData})
+       //const {posts} = this.state;
+     }
+    
+    render(){
+             
+        const {posts} = this.state;
+        //const {assetsDatas} = this.state;
+
+        //console.log(posts)
+        //console.log(assetsDatas)
+
+        const centreValues = Object.entries(posts).map(([title, value]) => ({ title, value}))
+
+        console.log(centreValues)
+      
+
+        return (
+        <div> 
+        <ul>
+            {posts.map(post => (
+                <li key={post.name}>
+                    Nome: {post.name} Resolvidos: {post.data.insightsChecked} 
+                   
+                    {//console.log(post.data.assetsData)}
+    }
+                </li> 
+            ))}
+        </ul>
+
+        </div>
+        )
+
+ }
+
+    
 }
 
+
+
 export default App;
+
+
